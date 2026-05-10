586589df88905ffdabaafdbb20db350e4521fd15
ahmadsss91123 <150787872+ahmadsss91123@users.noreply.github.com>
2026-05-10 12:03:42 +0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -3,7 +3,7 @@
 // GANTI nilai di bawah ini dengan credentials Supabase Anda
 // ============================================================
 
-const SUPABASE_URL = 'https://giqynajbhchsqevbxhsi.supabase.co/rest/v1/';
+const SUPABASE_URL = 'https://giqynajbhchsqevbxhsi.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdpcXluYWpiaGNoc3FldmJ4aHNpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzNzU4MjcsImV4cCI6MjA5Mzk1MTgyN30.-IxewgNlk-ya7XjkPeVk2yiiA1KZ7U_LrmrRJjGcVIs';
 
 // Inisialisasi Supabase client menggunakan CDN
